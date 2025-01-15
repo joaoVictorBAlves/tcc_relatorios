e182a64fb95b3e612395af8ee7bd5e126bc341d7
João Victor <joaovba2002@alu.ufc.br>
2025-01-15 16:33:17 -0300

    ✨Feat: Add ContextForm component to Devolutivas and Relatorios pages with updated layout and styling

--- src/pages/Devolutivas/index.jsx
+++ src/pages/Devolutivas/index.jsx
@@ -1,12 +1,30 @@
+import { Box, Typography } from "@mui/material";
+import RelatorioLogo from "../../assets/relatorio_logo";
 import NavBar from "../../components/Navbar";
+import ContextForm from "../../components/ContextForm";
 
 const Devolutivas = () => {
     return (
         <div id="devolutivas">
-        <NavBar/>
+            <NavBar />
+            <Box margin="0 auto" display={"flex"} flexDirection={"column"} alignItems={"center"} paddingTop={8} width="fit-content">
+                <RelatorioLogo />
+                <Typography variant="h6" component="div" sx={{
+                    'font-family': 'Poppins',
+                    'font-size': '18px',
+                    'font-weight': 600,
+                    'line-height': '26px',
+                    'text-align': 'center',
+                    'text-underline-position': 'from-font',
+                    'text-decoration-skip-ink': 'none',
 
+                }}>
+                    Para visualizar uma devolutiva, escolha um contexto:
+                </Typography>
+            </Box>
+            <ContextForm />
         </div>
     );
 }
- 
+
 export default Devolutivas;
--- src/pages/Relatorios/index.jsx
+++ src/pages/Relatorios/index.jsx
@@ -1,12 +1,30 @@
+import { Box, Typography } from "@mui/material";
+import RelatorioLogo from "../../assets/relatorio_logo";
+import ContextForm from "../../components/ContextForm";
 import NavBar from "../../components/Navbar";
 import "./Relatorios.css"
 const Relatorios = () => {
     return (
         <div id="relatorios">
-        <NavBar/>
+            <NavBar />
+            <Box margin="0 auto" display={"flex"} flexDirection={"column"} alignItems={"center"} paddingTop={8} width="fit-content">
+                <RelatorioLogo />
+                <Typography variant="h6" component="div" sx={{
+                    'font-family': 'Poppins',
+                    'font-size': '18px',
+                    'font-weight': 600,
+                    'line-height': '26px',
+                    'text-align': 'center',
+                    'text-underline-position': 'from-font',
+                    'text-decoration-skip-ink': 'none',
 
+                }}>
+                    Para visualizar um relatório, escolha um contexto:
+                </Typography>
+            </Box>
+            <ContextForm />
         </div>
     );
 }
- 
+
 export default Relatorios;
